d4d4d1462abef060ada741b3a8e672c2513120d2
mjaben <alexaben291@gmail.com>
2026-07-15 18:14:49 +0100

    chore: fix render build by ignoring dist and disabling incremental build

--- backend/dist/src/uploads/uploads.controller.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class UploadsController {
-    uploadImage(file: Express.Multer.File): {
-        url: string;
-    };
-    uploadVideo(file: Express.Multer.File): Promise<unknown>;
-}
